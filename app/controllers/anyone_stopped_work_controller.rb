1f71d18944d75465b7af3cca4a14a7e0eee29603
Whitney Schaefer <whitney@codeforamerica.org>
2018-12-14 10:50:11 -0800

    inherit from YesNoFormsController

--- app/controllers/anyone_stopped_work_controller.rb
+++ app/controllers/anyone_stopped_work_controller.rb
@@ -1,8 +1,4 @@
-class AnyoneStoppedWorkController < FormsController
-  layout "yes_no"
-
-  helper_method :yes_no_method_name
-
+class AnyoneStoppedWorkController < YesNoFormsController
   def yes_no_method_name
     :anyone_stopped_work
   end
