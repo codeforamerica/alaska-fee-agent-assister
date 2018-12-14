44683062b2394b3c9165e49591e4c538afd6ad5b
Andrew Hyder <hyd415@gmail.com>
2018-12-14 15:16:16 -0800

    Only ask about CAMA if they selected CAMA

--- app/controllers/cama_details_controller.rb
+++ app/controllers/cama_details_controller.rb
@@ -1,3 +1,7 @@
 class CamaDetailsController < FormsController
   layout "left_aligned"
+
+  def self.show_rule_sets(interview)
+    super << interview.selected_cama?
+  end
 end
--- spec/controllers/cama_details_controller_spec.rb
+++ spec/controllers/cama_details_controller_spec.rb
@@ -5,5 +5,24 @@ RSpec.describe CamaDetailsController do
   it_behaves_like "form controller successful update", {
     cama_details: "Best E. Person",
   }
-  it_behaves_like "form controller always shows"
+
+  describe "#show?" do
+    context "when applying or renewing for cama" do
+      it "returns true" do
+        application = create(:interview, selected_cama: true)
+
+        show = CamaDetailsController.show?(application)
+        expect(show).to eq(true)
+      end
+    end
+
+    context "when not applying for cama" do
+      it "returns false" do
+        application = create(:interview, selected_cama: false)
+
+        show = CamaDetailsController.show?(application)
+        expect(show).to eq(false)
+      end
+    end
+  end
 end
